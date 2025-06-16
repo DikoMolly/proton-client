eddac80cd6673f5be9e6d20a39145ffc6270edc5
Emmanuel-Diko <emmanuelnnachi@punch.agency>
2025-06-16 06:25:37 -0700

    changed the endpoint url to the hosted render

--- src/services/productservice.js
+++ src/services/productservice.js
@@ -4,7 +4,7 @@ export const searchProducts = async (query) => {
   if (!query.trim()) return [];
 
   try {
-    const res = await axios.post('http://localhost:8000/api/products/search', {
+    const res = await axios.post('https://proton-server-mbbk.onrender.com/api/products/search', {
       description: query,
     });
     return res.data.results || [];
